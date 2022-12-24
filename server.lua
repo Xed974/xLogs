4122b38f1bda68309855790ad41dcbfcfe846c3e
Xed <93364057+Xed974@users.noreply.github.com>
2022-12-24 14:32:12 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,11 @@
-local date = os.date('*t')
-
-if date.day < 10 then date.day = '0' .. tostring(date.day) end
-if date.month < 10 then date.month = '0' .. tostring(date.month) end
-if date.hour < 10 then date.hour = '0' .. tostring(date.hour) end
-if date.min < 10 then date.min = '0' .. tostring(date.min) end
-if date.sec < 10 then date.sec = '0' .. tostring(date.sec) end
-
 local function sendToDiscordWithSpecialURL(Name, Title, Description, Webhook)
+	local date = os.date('*t')
+
+	if date.day < 10 then date.day = '0' .. tostring(date.day) end
+	if date.month < 10 then date.month = '0' .. tostring(date.month) end
+	if date.hour < 10 then date.hour = '0' .. tostring(date.hour) end
+	if date.min < 10 then date.min = '0' .. tostring(date.min) end
+	if date.sec < 10 then date.sec = '0' .. tostring(date.sec) end
 	local Content = {
 	        {
 	            ["color"] = 0,
